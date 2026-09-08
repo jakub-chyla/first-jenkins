03fbcde4419a37c1cea8868a8c6208e0c52a9808
jakub-chyla <jakub.bchyla@gmail.com>
2026-09-08 18:45:17 +0200

    added controller

---
+++ src/main/java/com/first_jenkins/first_jenkins/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.first_jenkins.first_jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello world!";
+    }
+}

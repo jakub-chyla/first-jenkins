f5aa9e55d08c608d9664d79571658b1be5dcfce6
jakub-chyla <jakub.bchyla@gmail.com>
2026-09-08 18:38:56 +0200

    first commit

---
+++ src/main/java/com/first_jenkins/first_jenkins/FirstJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.first_jenkins.first_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJenkinsApplication.class, args);
+	}
+
+}
